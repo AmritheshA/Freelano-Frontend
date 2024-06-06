fae482a3faf67feff98464453eb336943aeb7073
AmritheshA <amrithesh0000@gmail.com>
2024-06-06 15:20:05 +0530

    fix:date wise seperation 1.6

--- src/Component/Freelancer/Chat/Messages.tsx
+++ src/Component/Freelancer/Chat/Messages.tsx
@@ -173,12 +173,11 @@ const formatDateAndConvert = (date: Date) => {
 
 const getDate = (date: Date) => {
 
-    if (!date || !(date instanceof Date) || isNaN(date.getTime())) {
-        date = new Date();
-    }
+    console.log("🚀 ~ getDate ~ date:", date)
+
 
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     return `${day}/${month}/${year}`;
 
