17bed2f969700957c34cb45a1afca714898ad72a
AmritheshA <amrithesh0000@gmail.com>
2024-06-08 13:31:25 +0530

    fix:chat profile icon update

--- src/Component/Freelancer/Chat/Messages.tsx
+++ src/Component/Freelancer/Chat/Messages.tsx
@@ -77,7 +77,7 @@ function Messages() {
                                                 <div className="chat chat-start">
                                                     <div className="chat-image avatar">
                                                         <div className="w-10 rounded-full">
-                                                            <img alt="Tailwind CSS chat bubble component" src={freelancerDetails.profileImgUrl} />
+                                                            <img alt="Tailwind CSS chat bubble component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                                                         </div>
                                                     </div>
                                                     <div className="chat-bubble chat-bubble-accent min-w-[120px] max-w-[300px]">
