421102dce2792e46ba7979fdb50adc0177922058
AmritheshA <amrithesh0000@gmail.com>
2024-06-08 16:46:48 +0530

    feat: online user1.7

--- src/Context/MessageContext/MessageProvider.tsx
+++ src/Context/MessageContext/MessageProvider.tsx
@@ -55,6 +55,18 @@ export const MessageProvider: React.FC<MessageProviderProps> = ({ children }) =>
 
 
   useEffect(() => {
+
+    const getOnlineUser = async() => {
+      const response = await axiosInstance.get("/api/v1/chat/getOnlineContacts")
+
+      const data = response.data;
+      console.log("🚀 ~ ...getOnlineUser ~ data:", data)
+      
+    }
+
+  }, [onlineUsers]);
+
+  useEffect(() => {
     const socket = new SockJS(`${import.meta.env.VITE_LOCALHOST_URL}`);
     const stompClient = Stomp.over(socket);
     if (user) {
